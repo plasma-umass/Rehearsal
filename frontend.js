b4dfda074f104f3c6cef8b8dc23725cd43021fb5
Arjun Guha <arjun@cs.umass.edu>
2016-06-06 12:40:59 -0400

    Trying out CORS

---
+++ frontend.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function onSubmit(_) {
+  var xhr = new XMLHTTPRequest();
+  xhr.open("POST", "http://104.196.37.39:5000/rehearsal", true)
+  xhr.send();
+}
+
+document.getElementById("submit").addEventListener("click", onSubmit);
