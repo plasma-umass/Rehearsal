b7a6876d18d47db632aba10185671120a884ebd5
Arjun Guha <arjun@cs.umass.edu>
2016-06-06 18:56:13 -0400

    forgot to send

--- frontend.js
+++ frontend.js
@@ -1,9 +1,11 @@
 "use strict";
 
+var code = document.getElementById("code");
+
 function onSubmit(_) {
   var xhr = new XMLHttpRequest();
   xhr.open("POST", "http://104.196.37.39:5000/rehearsal", true)
-  xhr.send();
+  xhr.send(code.value);
 }
 
 document.getElementById("submit").addEventListener("click", onSubmit);
